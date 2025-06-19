bff5d8ff62db9c49db6b7582274c46a2806cced8
leocassiosilva <leocassiosilva1234@gmail.com>
2025-06-19 18:51:22 -0300

    conflitos

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,14 +1,7 @@
 import { StyleSheet } from "react-native";
 
-<<<<<<< HEAD
-<<<<<<< HEAD:src/app/index/styles.ts
 import { colors} from '@/styles/colors';
-=======
-import { colors} from '../styles/colors';
->>>>>>> aab65e4 (fix: corrigindo arquivos e pastas da aplicação):src/app/styles.ts
-=======
-import { colors} from '@/styles/colors';
->>>>>>> 2deb8e8 (resolvendo conflitos)
+
 
 export const styles = StyleSheet.create({
     container: {
