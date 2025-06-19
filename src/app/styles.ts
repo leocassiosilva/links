135abcd4cfad69251f1e05bc6086b8c965a82d3d
leocassiosilva <leocassiosilva1234@gmail.com>
2025-06-19 18:47:08 -0300

    merge

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,6 +1,10 @@
 import { StyleSheet } from "react-native";
 
+<<<<<<< HEAD:src/app/index/styles.ts
 import { colors} from '@/styles/colors';
+=======
+import { colors} from '../styles/colors';
+>>>>>>> aab65e4 (fix: corrigindo arquivos e pastas da aplicação):src/app/styles.ts
 
 export const styles = StyleSheet.create({
     container: {
