022603bd9cfaf2de25bf06136b676db18087c620
leocassiosilva <leocassiosilva1234@gmail.com>
2025-06-20 19:31:51 -0300

    aplicando variação de estilo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -14,8 +14,9 @@ export default function Index() {
                 <MaterialIcons name='add' size={32} color={colors.green[300]} />
             </TouchableOpacity>
         </View>
-        <Category name="Projeto" icon="code" />
-        <Category name="Teste" icon="code" />
+        <Category name="Projeto" icon="code" isSlected/>
+        <Category name="Site" icon="language" isSlected={false}/>
+        <Category name="Video" icon="movie" isSlected={false}/>
     </View>
 
     );
--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -5,15 +5,18 @@ import { styles } from './styles';
 
 type Props = PressableProps & {
     name: string
+    isSlected: boolean
     icon: keyof typeof MaterialIcons.glyphMap
 }
 
-export function Category({ name, icon , ...rest}: Props) {
+export function Category({ name, icon , isSlected, ...rest}: Props) {
+    const color = isSlected ? colors.green[300] : colors.gray[400];
+    
     return (
 
         <Pressable style={styles.container}>
-            <MaterialIcons name={icon} size={16} color={colors.gray[400]}/>
-            <Text style={styles.name}>{name}</Text>
+            <MaterialIcons name={icon} size={16} color={color}/>
+            <Text style={[styles.name, {color}]}>{name}</Text>
         </Pressable>
     )
 }
