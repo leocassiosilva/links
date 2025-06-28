d17dabf12ab014332d4232e2f39df6543e4a1d5c
leocassiosilva <leocassiosilva1234@gmail.com>
2025-06-28 10:06:13 -0300

    Métodos de Listar e Salvar

---
+++ src/storage/link-storage.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LINK_STORAGE_KEY = "links-storage";
+
+type LinksStorage = {
+    id: string 
+    name: string
+    url: string
+    category: string
+} 
+
+async function get(){
+    const storage = await AsyncStorage.getItem(LINK_STORAGE_KEY);
+    const response = storage ? JSON.parse(storage) : [] 
+    return response ;
+
+}
+
+async function save(newLink: LinksStorage[]) {
+    try {
+        const storage = await get()
+        const updated = JSON.stringify([...storage, newLink])
+        await AsyncStorage.setItem(LINK_STORAGE_KEY, updated)
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+export const linkStorage = {get, save}
